3f337c2a4718ef832e9ac171e0af6c344f00f8ab
Zulfabasmallahagustyn <41808285+Zulfabasmallahagustyn@users.noreply.github.com>
2018-11-25 12:07:20 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { OnPress, AppRegistry, Button, View, TextInput, StyleSheet, Image, Style, Text } from 'react-native';
+
+export default class login extends Component {
+  render() {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+}"# login_reactnative" 
